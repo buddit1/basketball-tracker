9b2ecdaa4b6fed5a0e5618dccd9189a858c23f45
Tim Budding <tim.budding@gmail.com>
2024-05-17 13:42:18 -0400

    cli args for backend

--- receive_video.py
+++ receive_video.py
@@ -2,16 +2,18 @@ import cv2 as cv
 import socket
 import struct
 import numpy as np
+import argparse
 
 
 
 
 
-def main():
+
+def main(args):
     # Set up socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_ip = '0.0.0.0'
-    port = 9999
+    host_ip = args.ip
+    port = args.port
     socket_address = (host_ip, port)
     server_socket.bind(socket_address)
     server_socket.listen(5)
@@ -54,4 +56,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="backend to track basketballs in videos.")
+    parser.add_argument('-ip', type=str, help='ip address to host service on.')
+    parser.add_argument('-port', type=int, help='port to listen for incoming connections on.')
+    parser.add_argument('-model', type=str, help='yolo variant to use for tracking.',
+                        choices=['yolov9c', 'yolov9e', 'yolov8n', 'yolov8s', 'yolov8m', 'yolov8l', 'yolov8x']
+                        )
+    args = parser.parse_args()
+    main(args)
